ab1a4f992c753cc4b2fee2a5a9ff4e5ba80f4d6a
Ugur <ugurbd@gmail.com>
2019-12-05 21:47:11 +0300

    Create 3Sum_2.java

---
+++ 3Sum_2.java
@@ -0,0 +1,27 @@
+// Time Limit Exceeded Error
+
+class Solution {   
+    public List<List<Integer>> threeSum(int[] nums) 
+    {
+        List<List<Integer>> result = new ArrayList();
+        Arrays.sort(nums);
+        
+        for(int i = 0; i < nums.length; ++i)
+            for(int j = i + 1; j < nums.length; ++j)
+                for(int k = j + 1; k < nums.length; ++k)
+                {
+                    List<Integer> temp = new ArrayList();
+                    int sum = nums[i]+nums[j]+nums[k];
+                    if(sum  == 0){
+                        temp.add(nums[i]);
+                        temp.add(nums[j]);
+                        temp.add(nums[k]);
+                        
+                        if(!result.contains(temp))
+                            result.add(temp);
+                    }
+                }
+        
+        return result;
+    }
+}
